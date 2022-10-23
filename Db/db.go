6eeeac3490e0ea8f36e713aed30734ac1d4f0bc7
msalbrain <salmanmusa2020@gmail.com>
2022-10-23 16:29:07 +0100

    initial commit

---
+++ Db/db.go
@@ -0,0 +1,131 @@
+package Db
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+)
+
+
+
+
+
+// This is a user defined method to close resources.
+// This method closes mongoDB connection and cancel context.
+func close(client *mongo.Client, ctx context.Context,
+	cancel context.CancelFunc) {
+
+	// CancelFunc to cancel to context
+	defer cancel()
+
+	// client provides a method to close
+	// a mongoDB connection.
+	defer func() {
+
+		// client.Disconnect method also has deadline.
+		// returns error if any,
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+// This is a user defined method that returns mongo.Client,
+// context.Context, context.CancelFunc and error.
+// mongo.Client will be used for further database operation.
+// context.Context will be used set deadlines for process.
+// context.CancelFunc will be used to cancel context and
+// resource associated with it.
+
+func connect(uri string) (*mongo.Client, context.Context,
+	context.CancelFunc, error) {
+
+	// ctx will be used to set deadline for process, here
+	// deadline will of 30 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(),
+		30*time.Second)
+
+	// mongo.Connect return mongo.Client method
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
+
+// This is a user defined method that accepts
+// mongo.Client and context.Context
+// This method used to ping the mongoDB, return error if any.
+func ping(client *mongo.Client, ctx context.Context) error {
+
+	// mongo.Client has Ping to ping mongoDB, deadline of
+	// the Ping method will be determined by cxt
+	// Ping method return error if any occurred, then
+	// the error can be handled.
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("connected successfully")
+	return nil
+}
+
+// query is user defined method used to query MongoDB,
+// that accepts mongo.client,context, database name,
+// collection name, a query and field.
+
+//  database name and collection name is of type
+// string. query is of type interface.
+// field is of type interface, which limits
+// the field being returned.
+
+// query method returns a cursor and error.
+func query(client *mongo.Client, ctx context.Context,
+	dataBase, col string, query, field interface{}) (result *mongo.Cursor, err error) {
+
+	// select database and collection.
+	collection := client.Database(dataBase).Collection(col)
+
+	// collection has an method Find,
+	// that returns a mongo.cursor
+	// based on query and field.
+	result, err = collection.Find(ctx, query,
+		options.Find().SetProjection(field))
+	return
+}
+
+
+
+
+func CheckDb(prodId string) ([]bson.D){
+
+	client, ctx, cancel, err := connect("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	defer close(client, ctx, cancel)
+
+    var filter, option interface{}
+     
+    filter = bson.D{
+        {Key: "dp_name", Value: prodId},
+    }
+     
+	cursor, err := query(client, ctx, "bot1",
+		"cache", filter, option)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var results []bson.D
+
+	if err := cursor.All(ctx, &results); err != nil {
+		panic(err)
+	}
+
+	return results	
+}
+
+
---
+++ main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+
+	// "os"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gorilla/websocket"
+	"github.com/msalbrain/goWsocket.git/Db"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+type Msg struct {
+	msg string
+}
+
+type CintrData struct {
+	AuthorIDFromFromMerchant string
+	AuthorName               string
+	Product                  string
+	Rating                   int
+	CreatedAt                string
+	Text                     string
+	Title                    string
+}
+
+type Sentiment struct {
+	Neg      float32
+	Neu      float32
+	Pos      float32
+	Compound float32
+}
+
+type Word struct {
+	Word       string
+	Total      int
+	Review_ids []string
+}
+
+type WSocketReturn struct {
+	Product           string
+	Bert              string
+	Pegasus           string
+	Textrank          string
+	Overall_sentiment Sentiment
+	NLP_rating        float32
+	Average_rating    float32
+	Count             int
+	Limit             int
+	Skip              int
+	Next              interface{}
+	Prev              interface{}
+	Keyword           []Word
+	Adj               []Word
+	Verb              []Word
+	Data              []CintrData
+}
+
+type CintrResult struct {
+	Count  int
+	Data   []CintrData
+	Status int
+}
+
+type PartialError struct {
+	Error string
+}
+
+type PrivateApi struct {
+	Msg    string
+	Detail PartialError
+	Status int
+}
+
+type DataInfo struct {
+	ProdId string
+	Skip   int
+	Limit  int
+}
+
+func getCintrData(prodId string, limit int, skip int) CintrResult {
+	url := fmt.Sprintf("https://cintr.herokuapp.com/api/reviews?productId=%s&limit=%d&skip=%d", prodId, limit, skip)
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		// os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	var d CintrResult
+	err = json.Unmarshal(responseData, &d)
+
+	return d
+}
+
+type SprocessReturn struct {
+	ws  WSocketReturn
+	pri PrivateApi
+}
+
+func startProcess(prodId string, limit int, skip int) SprocessReturn {
+
+	res := Db.CheckDb(prodId)
+	is := false
+	var anlysis map[string]interface{}
+	for _, doc := range res {
+		ma := doc.Map()
+		if ma["limit"] == limit && ma["skip"] == skip {
+			is = true
+			anlysis = ma
+		}
+
+	}
+
+	if is == true {
+		var cintrcompose WSocketReturn
+		cintrData := getCintrData(prodId, limit, skip)
+		cintrcompose.Data = cintrData.Data
+		cintrcompose.Count = cintrData.Count
+		cintrcompose.Product = anlysis["product"].(string)
+		cintrcompose.Bert = anlysis["bert"].(string)
+		cintrcompose.Pegasus = anlysis["pegasus"].(string)
+		cintrcompose.Textrank = anlysis["textrank"].(string)
+		cintrcompose.NLP_rating = anlysis["NLP_rating"].(float32)
+		cintrcompose.Average_rating = anlysis["Average_rating"].(float32)
+		if limit == 0 {
+			cintrcompose.Limit = cintrData.Count
+		} else {
+			cintrcompose.Limit = limit
+		}
+		cintrcompose.Skip = anlysis["skip"].(int)
+
+		return SprocessReturn{ws: cintrcompose}
+
+	} else {
+		link := fmt.Sprintf("http://3.235.109.178/private/api/reviews/?productId=%s&limit=%v&skip=%v&access_token=85e53c1f044bb27455557fd3cdf",
+			prodId, limit, skip)
+
+		response, err := http.Get(link)
+
+		if err != nil {
+			log.Println(err.Error())
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		fmt.Println(string(responseData))
+		var d PrivateApi
+		err = json.Unmarshal(responseData, &d)
+		d.Status = response.StatusCode
+		return SprocessReturn{pri: d}
+	}
+
+}
+
+func WSocketReply(c *websocket.Conn, Val DataInfo) {
+
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+
+	defer c.Close()
+	for {
+		var V map[string]interface{}
+		err := c.ReadJSON(&V)
+
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+
+		m := Msg{msg: V["msg"].(string)}
+
+		log.Printf("recv: %v", m.msg)
+
+		err = c.WriteJSON(map[string]interface{}{"said": "fuck you", "age": 4})
+		// err = c.WriteMessage(mt, jsonMap)
+		// err = c.WriteMessage(websocket.TextMessage, stringJSON)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	// flag.Parse()
+	// log.SetFlags(0)
+	// http.HandleFunc("/echo", echo)
+	// log.Fatal(http.ListenAndServe(*addr, nil))
+	// c := startProcess("63179a3bc987724acf03ddc8", 4, 4)
+	// fmt.Println(c)
+	// getCintrData("63179a3bc987724acf03ddc8", 4, 4)
+	
+	fmt.Println(startProcess("63179a3bc987724acf03ddc8", 2, 0))
+}
