27fb8e1e1e3cadb70d9dc0d4bce9e9b656a9864a
msalbrain <salmanmusa2020@gmail.com>
2022-10-23 17:24:04 +0100

    tenth commit

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ type WSocketReturn struct {
 	Bert              string
 	Pegasus           string
 	Textrank          string
-	Overall_sentiment Sentiment
+	Overall_sentiment map[string]interface{}
 	NLP_rating        float64
 	Average_rating    float64
 	Count             int
@@ -61,7 +61,7 @@ type WSocketReturn struct {
 	Keyword           []Word
 	Adj               []Word
 	Verb              []Word
-	// Data              []CintrData
+	Data              []map[string]interface{}
 }
 
 type CintrResult struct {
